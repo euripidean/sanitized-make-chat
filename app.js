8358b2f1be8ad57f3d0d72329aee6e864f9dd0b0
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2023-11-06 15:25:00 -0800

    Added DOMPurify to inputs

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 //app.js
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
+const server = require("http").Server(app);
 
 //Socket.io
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 //We'll store our online users here
 let onlineUsers = {};
-let channels = {"General" : []};
+let channels = { General: [] };
 io.on("connection", (socket) => {
   // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
-})
+  require("./sockets/chat.js")(io, socket, onlineUsers, channels);
+});
 
 //Handlebars
-const exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // use public folder
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render('index.handlebars');
-})
+app.get("/", (req, res) => {
+  res.render("index.handlebars");
+});
 
-server.listen('3000', () => {
-  console.log('Server listening on Port 3000');
-})
+server.listen("3000", () => {
+  console.log("Server listening on Port 3000");
+});
